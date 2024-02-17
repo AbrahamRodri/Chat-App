996fdf9812fc8a4044f8ada03357f03388fe0296
Abraham Rodriguez <108596161+AbrahamRodri@users.noreply.github.com>
2024-02-17 09:31:53 -0800

    mongoose updated , generateToken

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,52 +1,60 @@
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
-    try {
-        const {fullName , username, password, confirmPassword, gender} = req.body;
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
 
-        if(password !== confirmPassword){
-            return res.status(400).json({error:"Passwords dont't match"})
-        }
-
-        const user = await User.findOne({username});
-
-        if(user){
-            return res.status(400).json({error:"Username already exists"})
-        }
-
-        // HASH PASSWORD HERE
-
-        // https://avatar-placeholder.iran.liara.run/
-
-        const profilePic = `https://avatar-placeholder.iran.liara.run/public/${gender}?username=${username}`;
-
-        const newUser = new User({
-            fullName,
-            username,
-            password,
-            gender,
-            profilePic
-        })
-
-        await newUser.save();
-
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
-
-    } catch (error) {
-        console.log("Error in signup controller", error.message);
-        res.status(500).json({error:"Internal Server Error"});
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords dont't match" });
     }
-}
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // HASH PASSWORD HERE
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // https://avatar-placeholder.iran.liara.run/
+
+    const profilePic = `https://avatar-placeholder.iran.liara.run/public/${gender}?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export const logout = (req, res) => {
-    console.log("logoutUser");
-}
+  console.log("logoutUser");
+};
 
 export const login = (req, res) => {
-    console.log("loginUser");
-}
+  console.log("loginUser");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -9,19 +9,18 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json()); 
+app.use(express.json());
 
-app.use("/api/auth" , authRoutes);
+app.use("/api/auth", authRoutes);
 
 // app.get("/", (req, res) => {
 
-    // root route http://localhost:5000/
-    // res.send("Hello World!!");
+// root route http://localhost:5000/
+// res.send("Hello World!!");
 
 // });
 
-
 app.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`server Running on port ${PORT}`)
+  connectToMongoDB();
+  console.log(`server Running on port ${PORT}`);
 });
